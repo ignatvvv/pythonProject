52493bf9ef4cf4435f4ab1babede27af3ed2469e
ignatvvv <vl-vl80@yandex.ru>
2023-02-08 20:35:24 +0300

    HW3

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-a = int(input())
-n = 2
-while a!= n and a>= n:
-    n = n * 2
-if a == n:
-    print("YES")
-else:
-    print("NO")
+s = input()
+digits = 0
+letters = 0
+for i in range(len(s)):
+    if s[i].isalpha():
+        letters = letters + 1
+    if s[i].isdigit():
+        digits = digits + 1
+print(letters)
+print(digits)
+
+
+
+
+
+
 
 
 
