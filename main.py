eebc3188821582fc82c269b83985306fc17668f1
ignatvvv <vl-vl80@yandex.ru>
2023-02-15 11:21:00 +0300

    hw 4

---
+++ main.py
@@ -0,0 +1,24 @@
+li = [5]
+a = [1, 5, 2, 4, 3]
+for i in range(len(a)-1):
+    n = int(a[i])
+    i = i + 1
+    m = int(a[i])
+    if m > n:
+        n = m
+        print(m, end=' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
